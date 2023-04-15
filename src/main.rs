30361400972d28b9c758af55df0f938bb2286a2d
Med_Ali_Khaled <72525144+MEDALIALPHA331@users.noreply.github.com>
2023-04-15 23:39:17 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    pretty_print("hello"); 
+}
+
+
+pub fn pretty_print(s: &str) {
+    print!(r"oh my god {}", s);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::pretty_print;
+
+    #[test]
+    fn test_pretty_print() {
+        assert_eq!((), pretty_print("hello"));
+    }
 }
